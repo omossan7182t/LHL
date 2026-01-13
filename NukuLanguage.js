da257dcfe726cfd6039bd6c3933a2cedbb838cab
omossan7182t <omossan7182t@gmail.com>
2026-01-13 10:45:20 +0900

    Update NukuLanguage.js

--- NukuLanguage.js
+++ NukuLanguage.js
@@ -1,7 +1,6 @@
 export const NukuLanguage = {
   id: "nuku",
   name: "ぬく方言",
-  unofficial: true,
   commands: {
     "+": { op: "ADD", delta: 1 },
     "-": { op: "ADD", delta: -1 },
@@ -10,9 +9,10 @@ export const NukuLanguage = {
     ".": { op: "OUTPUT" },
     ",": { op: "INPUT" },
     "[": { op: "LOOP_START" },
-    "]": { op: "LOOP_END" },
+    "]": { op: "LOOP_END" }
   },
   samples: {
-    helloWorld: `ぬくぬくぬく ぬく ぬく ぬくく ぬくぬくっ...` // 実際のHello Worldコードは後で設定
+    helloWorld: `ぬくぬくぬく ぬくく ぬくぬくっ`,
+    demoLoop: `ぬくぬく ぬくっ ぬくぬくぬく`
   }
 };
