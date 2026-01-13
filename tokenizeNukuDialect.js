28026b7a036fe34f6d3c9cab19d57bb942311f5c
omossan7182t <omossan7182t@gmail.com>
2026-01-13 10:53:38 +0900

    Update tokenizeNukuDialect.js

--- tokenizeNukuDialect.js
+++ tokenizeNukuDialect.js
@@ -1,4 +1,4 @@
-import { LanguageRegistry } from "./LanguageRegistry.js";
+import { LanguageRegistry } from "../language/LanguageRegistry.js";
 
 export function tokenizeNukuDialect(src, langId = "nuku") {
   const lang = LanguageRegistry.get(langId);
@@ -7,23 +7,12 @@ export function tokenizeNukuDialect(src, langId = "nuku") {
   for (const ch of src) {
     const cmd = lang.commands[ch];
     if (!cmd) continue;
-    tokens.push({ op: cmd.op, delta: cmd.delta });
-  }
 
-  // ジャンプテーブル構築
-  const loopStack = [];
-  tokens.forEach((token, idx) => {
-    if (token.op === "LOOP_START") loopStack.push(idx);
-    if (token.op === "LOOP_END") {
-      if (loopStack.length === 0) token.jump = null; // ERROR later
-      else {
-        const startIdx = loopStack.pop();
-        token.jump = startIdx;
-        tokens[startIdx].jump = idx;
-      }
-    }
-  });
-  if (loopStack.length > 0) throw new Error("Unmatched loop");
+    tokens.push({
+      op: cmd.op,
+      delta: cmd.delta
+    });
+  }
 
   return tokens;
 }
